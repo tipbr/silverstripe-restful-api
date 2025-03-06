dd3a872abd16603dbdf848157acbfa22cd6766a4
warm--tape <bl.tipper@gmail.com>
2025-03-06 21:29:56 +1300

    WIP

--- src/Extensions/PasswordResetSecurityExtension.php
+++ src/Extensions/PasswordResetSecurityExtension.php
@@ -7,28 +7,20 @@ use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\GridField\GridField;
 use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
 use SilverStripe\Core\Extension;
+use SilverStripe\Forms\TabSet;
 use SilverStripe\Forms\Tab;
 use TipBr\DataObjects\PasswordResetRequest;
 
 class PasswordResetSecurityExtension extends Extension
 {
     /**
-     * Update CMS Fields to add PasswordResetRequest GridField
-     *
-     * @param FieldList $fields
+     * Add a tab to the SecurityAdmin
      */
     public function updateEditForm($form)
     {
-        // Get the form fields
         $fields = $form->Fields();
 
-        // Create a new tab for the password reset requests
-        $resetRequestsTab = Tab::create(
-            'PasswordResetRequests',
-            'Password Reset Requests'
-        );
-
-        // Create the gridfield for the requests
+        // Create gridfield for password reset requests
         $gridFieldConfig = GridFieldConfig_RecordEditor::create();
         $gridField = GridField::create(
             'PasswordResetRequests',
@@ -37,13 +29,56 @@ class PasswordResetSecurityExtension extends Extension
             $gridFieldConfig
         );
 
-        // Associate the gridfield with the form
-        $gridField->setForm($form);
+        // Find the main TabSet
+        $mainTabSet = $fields->fieldByName('Root');
 
-        // Add the gridfield to the tab
-        $resetRequestsTab->push($gridField);
+        if ($mainTabSet && $mainTabSet instanceof TabSet) {
+            // Create a new tab and add it directly to the main TabSet
+            $tab = Tab::create(
+                'PasswordResetRequests',
+                'Password Reset Requests',
+                $gridField
+            );
 
-        // Add the tab to the form
-        $fields->insertAfter('Users', $resetRequestsTab);
+            $mainTabSet->push($tab);
+        }
     }
 }
+
+// class PasswordResetSecurityExtension extends Extension
+// {
+//     /**
+//      * Update CMS Fields to add PasswordResetRequest GridField
+//      *
+//      * @param FieldList $fields
+//      */
+//     public function updateEditForm($form)
+//     {
+//         // Get the form fields
+//         $fields = $form->Fields();
+
+//         // Create a new tab for the password reset requests
+//         $resetRequestsTab = Tab::create(
+//             'PasswordResetRequests',
+//             'Password Reset Requests'
+//         );
+
+//         // Create the gridfield for the requests
+//         $gridFieldConfig = GridFieldConfig_RecordEditor::create();
+//         $gridField = GridField::create(
+//             'PasswordResetRequests',
+//             'Password Reset Requests',
+//             PasswordResetRequest::get(),
+//             $gridFieldConfig
+//         );
+
+//         // Associate the gridfield with the form
+//         $gridField->setForm($form);
+
+//         // Add the gridfield to the tab
+//         $resetRequestsTab->push($gridField);
+
+//         // Add the tab to the form
+//         $fields->insertAfter('Users', $resetRequestsTab);
+//     }
+// }
